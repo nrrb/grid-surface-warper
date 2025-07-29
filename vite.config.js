d836c7d71944a5d10ec5adfaae7d96a9ceeb816a
Nicholas Bennett <nicholasbennett555@gmail.com>
2025-07-28 21:11:06 -0400

    Add config to build for GH Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue(), vuetify()],
-  server: { port: 5190 }
-})
+  server: { port: 5190 },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    outDir: 'docs',
+  },
+  base: mode === 'production' ? '/grid-surface-warper/' : '/'    
+}))
